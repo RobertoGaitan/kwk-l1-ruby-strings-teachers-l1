01eed56a8db835b4f4f2f245c78d688d094f075f
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-01 17:51:12 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -9,12 +9,15 @@ party_name = gets.chomp.capitalize
 party_name = "Best Halloween Party Ever"
 
 puts "What is the date of the party?"
-time = gets.chomp.capitalize
+date = gets.chomp.capitalize
+
+puts "What is the time of the party?"
+time = gets.chomp
 
 puts "What is the name of the host?"
 host_name = gets.chomp.capitalize
 
 puts "Dear " + guest_name + ","
-puts "You are cordially invited to the " + party_name + " on " + time + ". Please RSVP no later than October 30."
+puts "You are cordially invited to the " + party_name + " on " + date + " at " + time + ". Please RSVP no later than October 30."
 puts "Sincerely,"
 puts host_name
